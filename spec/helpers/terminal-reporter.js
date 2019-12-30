a3e42b45ab3fad23cd957a275bf829b1ba5ff5a9
Kiss <arpad1337@me.com>
2019-12-30 12:21:30 +0100

    Changes

--- spec/helpers/terminal-reporter.js
+++ spec/helpers/terminal-reporter.js
@@ -1,12 +1,12 @@
-var Reporter = require('jasmine-console-reporter');
-var reporter = new Reporter({
-    colors: 1,           // (0|false)|(1|true)|2
-    cleanStack: 1,       // (0|false)|(1|true)|2|3
-    verbosity: 4,        // (0|false)|1|2|(3|true)|4|Object
-    listStyle: 'indent', // "flat"|"indent"
-    timeUnit: 'ms',      // "ms"|"ns"|"s"
-    timeThreshold: { ok: 500, warn: 1000, ouch: 3000 }, // Object|Number
-    activity: false,     // boolean or string ("dots"|"star"|"flip"|"bouncingBar"|...)
+const Reporter = require('jasmine-console-reporter');
+const reporter = new Reporter({
+    colors: 1,
+    cleanStack: 1,
+    verbosity: 4,
+    listStyle: 'indent',
+    timeUnit: 'ms',
+    timeThreshold: { ok: 500, warn: 1000, ouch: 3000 },
+    activity: false,
     emoji: true,
     beep: true
 });
